6b5708b3fa98d12c19eaf403a0b470ff49fc1a03
MrXyfir <mr@xyfir.com>
2017-02-26 20:50:26 -0800

    Add config command

---
+++ commands/config.js
@@ -0,0 +1,53 @@
+const getConfig = require('../lib/config/get');
+const setConfig = require('../lib/config/set');
+const cliTable = require('cli-table');
+const fs = require('fs');
+
+/**
+ * Based on the options provided, gets or sets config values.
+ * @param {yargs} yargs
+ */
+module.exports = async function(yargs) {
+
+  const argv = yargs.argv;
+
+  try {
+    const config = await getConfig();
+
+    // Set value for key
+    if (argv.key && argv.value) {
+      if (config[argv.key] !== undefined) {
+        config[argv.key] = argv.value;
+
+        await setConfig(config);
+        console.log('Success');
+      }
+      else {
+        console.error('Invalid key'.red);
+      }
+    }
+    // Return value for key
+    else if (argv.key) {
+      if (config[argv.key] !== undefined)
+        console.log(config[argv.key])
+      else
+        console.error('Invalid key'.red);
+    }
+    // Return all keys/values
+    else {
+      const output = new cliTable({
+        head: ['Key', 'Value'], colWidths: [35, 75]
+      });
+      
+      Object.entries(config).forEach(e => {
+        output.push([ e[0], e[1] ]);
+      });
+
+      console.log(output.toString());
+    }
+  }
+  catch (e) {
+    console.log(e.toString().red);
+  }
+
+}
