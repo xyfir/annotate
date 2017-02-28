8086efd4235ff75a9179d3687e14156959431a80
MrXyfir <mr@xyfir.com>
2017-02-27 19:30:45 -0800

    Add commands object

---
+++ commands/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  removeFromIgnoreList: require('./ignore-list/remove-from'),
+  addToIgnoreList: require('./ignore-list/add-to'),
+  resetIgnoreList: require('./ignore-list/reset'),
+  showIgnoreList: require('./ignore-list/show'),
+  generate: require('./generate'),
+  config: require('./config')
+}
