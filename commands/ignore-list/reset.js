51590ad92a9a33a1c1a67837838a8a62a6c72ee5
MrXyfir <mr@xyfir.com>
2017-02-27 19:02:15 -0800

    Add `ignore reset` command

---
+++ commands/ignore-list/reset.js
@@ -0,0 +1,20 @@
+const setIgnoreList = require('../../lib/ignore-list/set');
+const fs = require('fs');
+
+/**
+ * Wipes the ignore list array.
+ * @param {yargs} yargs
+ */
+module.exports = async function(yargs) {
+
+  yargs.argv;
+
+  try {
+    await setIgnoreList([]);
+    console.log('Success'.green);
+  }
+  catch (e) {
+    console.error(e.toString().red);
+  }
+
+}
