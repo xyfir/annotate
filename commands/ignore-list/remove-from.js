64b165de1a922daa5509dadd11a6c88ed7a2312f
MrXyfir <mr@xyfir.com>
2017-02-27 19:02:41 -0800

    Add `ignore remove` command

---
+++ commands/ignore-list/remove-from.js
@@ -0,0 +1,27 @@
+const setIgnoreList = require('../../lib/ignore-list/set');
+const getIgnoreList = require('../../lib/ignore-list/get');
+const fs = require('fs');
+
+/**
+ * Removes book ids from list.
+ * @param {yargs} yargs
+ */
+module.exports = async function(yargs) {
+
+  const argv = yargs.argv;
+
+  try {
+    if (!argv.ids) throw 'Missing values for --ids';
+
+    const ids = String(argv.ids).split(',');
+    let list = await getIgnoreList();
+    list = list.filter(id => ids.indexOf(id) == -1);
+
+    await setIgnoreList(list);
+    console.log('Success'.green);
+  }
+  catch (e) {
+    console.error(e.toString().red);
+  }
+
+}
