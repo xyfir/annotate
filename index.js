83f322866c5957dc7b7fce00bc7ffa7b13a1fb41
MrXyfir <mr@xyfir.com>
2017-06-10 20:27:01 -0700

    Improvements, changes
    - Use app-module-path
    - Clean up code
    - Move `generate` command to `generate calibre`
    - Add `generate libgen` command

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 #!/usr/bin/env node
+require('app-module-path').addPath(__dirname);
 require('colors');
+
 const commands = require('./commands');
 const yargs = require('yargs');
 
 yargs
   .command(
-    'generate', 'Generate annotations', commands.generate
+    'generate', 'Generate annotations',
+    () => yargs
+      .command(
+        'calibre',
+        'Generate annotations using books from your Calibre library',
+        commands.generateCalibre
+      )
+      .command(
+        'libgen',
+        'Generate annotations using books from Library Genesis',
+        commands.generateLibGen
+      )
   )
   .command(
     'config', 'Sets or gets config property values', commands.config
   )
-  .command('ignore', 'Manipulate ignore list', () => {
-    return yargs
+  .command(
+    'ignore', 'Manipulate ignore list',
+    () => yargs
       .command(
         'add', 'Add book id(s) to ignore list',
         commands.addToIgnoreList
@@ -27,9 +41,8 @@ yargs
       .command(
         'show', 'Shows contents of list',
         commands.showIgnoreList
-      );
-  })
-  .demand(1, 'You must provide a valid command')
+      )
+  )
   .help('h')
   .alias('h', 'help')
   .argv;
