05b13eb92fdb66ef5f3a70899d9a61e56d79d813
MrXyfir <mr@xyfir.com>
2017-06-10 20:30:38 -0700

    Rename generate to generateCalibre. Add generateLibGen

--- commands/index.js
+++ commands/index.js
@@ -2,7 +2,8 @@ module.exports = {
   removeFromIgnoreList: require('./ignore-list/remove-from'),
   addToIgnoreList: require('./ignore-list/add-to'),
   resetIgnoreList: require('./ignore-list/reset'),
+  generateCalibre: require('./generate/calibre'),
   showIgnoreList: require('./ignore-list/show'),
-  generate: require('./generate'),
+  generateLibGen: require('./generate/libgen'),
   config: require('./config')
 }
