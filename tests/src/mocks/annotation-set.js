ae2e8bdebb177382a37306db85c0b2f9c83c592c
MrXyfir <mr@xyfir.com>
2018-03-26 12:16:19 -0700

    Add annotation set mock

---
+++ tests/src/mocks/annotation-set.js
@@ -0,0 +1,15 @@
+export default {
+  id: 1,
+  items: [{
+    id: 1, title: 'Benjamin Franklin',
+    searches: [
+      {main: 'Benjamin Franklin'},
+      {main: 'Franklin'},
+      {main: 'FRANKLIN'}
+    ],
+    annotations: [{
+      type: 3, name: 'Search', value: 'Benjamin Franklin',
+      context: 'Autobiography'
+    }]
+  }]
+}
