a0376b197981d9d79ed90aea14fa5e4b889ee405
MrXyfir <mr@xyfir.com>
2018-03-25 20:36:58 -0700

    Fix output

--- epubjs/webpack.config.js
+++ epubjs/webpack.config.js
@@ -18,6 +18,7 @@ module.exports = {
   entry: './src/index.js',
 
   output: {
+    libraryTarget: 'umd',
     filename: 'index.js',
     library: 'XyfirAnnotateEPUBJS',
     path: path.resolve(__dirname, 'dist')
