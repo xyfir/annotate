b146733605c05723127a0f30841b7cc2561ab0a5
MrXyfir <mr@xyfir.com>
2018-03-26 15:06:08 -0700

    Add entry file

---
+++ html/src/index.js
@@ -0,0 +1,9 @@
+import buildSearchOrder from './build-search-order';
+import findMatchIndexes from './find-indexes';
+import wrapMatches from './wrap';
+
+export default {
+  buildSearchOrder,
+  findMatchIndexes,
+  wrapMatches
+};
