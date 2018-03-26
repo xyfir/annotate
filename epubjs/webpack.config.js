a96d25afaa0137211ad15aed63b2c3daa5dd9b8a
MrXyfir <mr@xyfir.com>
2018-03-25 18:51:29 -0700

    Add webpack.config.js for @xyfir/annotate-epubjs

---
+++ epubjs/webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const MODE = 'production';
+
+const plugins = MODE == 'production' ? [
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify('production')
+    }
+  })
+] : [];
+
+module.exports = {
+
+  mode: MODE,
+
+  entry: './src/index.js',
+
+  output: {
+    filename: 'index.js',
+    library: 'XyfirAnnotateEPUBJS',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'src'),
+      'node_modules'
+    ],
+    extensions: ['.js']
+  },
+
+  module: {
+    rules: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      include: [
+        path.resolve(__dirname, 'src/annotations'),
+        path.resolve(__dirname, 'src/matches')
+      ],
+      exclude: /node_modules/,
+      options: {
+        presets: ['env']
+      }
+    }]
+  },
+
+  plugins
+
+};
