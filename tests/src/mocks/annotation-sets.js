a99c9eb2b894cd33b9d5f292b0a46d0ba64802ce
MrXyfir <mr@xyfir.com>
2018-03-26 15:49:35 -0700

    Add more mock sets for testing different scenarios

--- tests/src/mocks/annotation-sets.js
+++ tests/src/mocks/annotation-sets.js
@@ -7,14 +7,17 @@
 export default [{
   id: 1,
   items: [{
-    id: 1, title: 'Benjamin Franklin',
+    id: 1,
+    title: 'Benjamin Franklin',
     searches: [
       {main: 'Benjamin Franklin'},
       {main: 'Franklin'},
       {main: 'FRANKLIN'}
     ],
     annotations: [{
-      type: 3, name: 'Search', value: 'Benjamin Franklin',
+      type: 3,
+      name: 'Search',
+      value: 'Benjamin Franklin',
       context: 'Autobiography'
     }]
   }],
@@ -24,4 +27,64 @@ export default [{
     'Franklin',
     'FRANKLIN'
   ]
-}]
+}, {
+  id: 2,
+  items: [{
+    id: 2,
+    title: 'EBook #20203',
+    searches: [{
+      main: 'EBook #[0-9]{5}',
+      regex: true
+    }],
+    annotations: [{
+      type: 2,
+      name: 'Link',
+      value: 'http://www.gutenberg.org/ebooks/20203'
+    }]
+  }],
+  elements: 1,
+  matches: [
+    'EBook #20203'
+  ]
+}, {
+  id: 3,
+  items: [{
+    id: 3,
+    title: 'Autobiography of Benjamin Franklin',
+    searches: [{
+      main: 'Autobiography of Benjamin Franklin',
+      after: 'Author:',
+      before: 'Title:'
+    }],
+    annotations: [{
+      type: 3,
+      name: 'Search',
+      value: 'Autobiography of Benjamin Franklin'
+    }]
+  }],
+  elements: 1,
+  matches: [
+    'Autobiography of Benjamin Franklin'
+  ]
+}, {
+  id: 4,
+  items: [{
+    id: 4,
+    title: 'Frank Woodworth Pine',
+    searches: [{
+      main: 'Frank .+ Pine',
+      regex: true,
+      after: 'IllustrT?ator:',
+      before: 'Edit(o|0)r:'
+    }],
+    annotations: [{
+      type: 2,
+      name: 'Link',
+      value: 'http://onlinebooks.library.upenn.edu/webbin/book/lookupname?key=Pine%2C%20Frank%20Woodworth%2C%201869-'
+    }]
+  }],
+  elements: 1,
+  matches: [
+    'Frank Woodworth Pine'
+  ]
+}];
