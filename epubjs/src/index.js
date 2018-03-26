cc69bf4d5a7021002607291906734cffe5591685
MrXyfir <mr@xyfir.com>
2018-03-25 18:50:19 -0700

    Add module to export all others

---
+++ epubjs/src/index.js
@@ -0,0 +1,15 @@
+import insertAnnotations from 'annotations/insert';
+import buildSearchOrder from 'annotations/build-search-order';
+import findMatchIndexes from 'matches/find-indexes';
+import unwrapMatches from 'matches/unwrap';
+import findMarkers from 'annotations/find-markers';
+import wrapMatches from 'matches/wrap';
+
+export default {
+  insertAnnotations,
+  buildSearchOrder,
+  findMatchIndexes,
+  unwrapMatches,
+  findMarkers,
+  wrapMatches
+};
