a65e5a9b9f69ecf8e10160bc04c866f1ab9d9f1e
MrXyfir <mr@xyfir.com>
2018-03-27 15:26:12 -0700

    Remove epubjs `unwrapMatches()`

--- epubjs/src/index.js
+++ epubjs/src/index.js
@@ -1,11 +1,9 @@
 import insertAnnotations from './insert';
-import unwrapMatches from './unwrap';
 import annotateHTML from 'repo/html';
 import findMarkers from './find-markers';
 
 export default {
   ...annotateHTML,
   insertAnnotations,
-  unwrapMatches,
   findMarkers
 };
