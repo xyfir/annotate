e31ab6c31cd1cf8abc431c8b507074a9443acb4f
MrXyfir <mr@xyfir.com>
2018-03-28 14:35:53 -0700

    Update how files are served. Import react-md styles

--- tests/src/server.js
+++ tests/src/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const server = express();
 const path = require('path');
 
-server.use('/dist', express.static(path.resolve(__dirname, '../dist')));
-server.use('/src', express.static(__dirname));
+server.use('/', express.static(path.resolve(__dirname, '../')));
 server.get('/', (rq, rs) => rs.sendFile(__dirname + '/index.html'));
 server.listen(2061, () => console.log('Server running'));
