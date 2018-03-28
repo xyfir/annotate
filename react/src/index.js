b37dde588e7a32837bda5326a4bbc5cb99ad75f1
MrXyfir <mr@xyfir.com>
2018-03-28 12:55:19 -0700

    Add entry file for @xyfir/annotate-react

---
+++ react/src/index.js
@@ -0,0 +1,18 @@
+import ViewAnnotations from './ViewAnnotations';
+import Document from './types/Document';
+import Search from './types/Search';
+import Image from './types/Image';
+import Video from './types/Video';
+import Audio from './types/Audio';
+import Link from './types/Link';
+import Map from './types/Map';
+
+export default {
+  ViewAnnotations,
+  Document,
+  Search,
+  Image,
+  Video,
+  Link,
+  Map
+};
