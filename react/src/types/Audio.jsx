1f7b8ee473a85679378977031a758d719ab5107a
MrXyfir <mr@xyfir.com>
2018-05-23 17:29:01 -0700

    Support new Audio annotation format

--- react/src/types/Audio.jsx
+++ react/src/types/Audio.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
 
-export default ({ annotation }) => (
-  <div className='audio-annotation'>
-    {(
-      Array.isArray(annotation.value) ? annotation.value : [annotation.value]
-    ).map(link =>
-      <audio src={link} key={link} controls />
-    )}
-  </div>
-);
+export default class AudioAnnotation extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      soundcloud: null,
+      badSource: false,
+      loading: true
+    };
+  }
+
+  componentDidMount() {
+    const { annotation } = this.props;
+    /** @type {string} */
+    const id = Array.isArray(annotation.value)
+      ? annotation.value[0]
+      : annotation.value;
+
+    switch (annotation.source) {
+      case 'soundcloud':
+        fetch(
+          `https://soundcloud.com/oembed` +
+            `?format=json` +
+            `&url=https://soundcloud.com/${id}`
+        )
+          .then(res => res.json())
+          .then(res => this.setState({ soundcloud: res, loading: false }));
+        break;
+      default:
+        this.setState({ badSource: true, loading: false });
+    }
+  }
+
+  render() {
+    const { soundcloud, badSource, loading } = this.state;
+    if (loading) return null;
+
+    return badSource ? (
+      <p>Cannot load audio track from this source.</p>
+    ) : (
+      <div
+        className="audio-annotation"
+        dangerouslySetInnerHTML={{ __html: soundcloud.html }}
+      />
+    );
+  }
+}
