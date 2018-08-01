79be5c4fd6ae08217b6f751046a942ce267b1f35
MrXyfir <mr@xyfir.com>
2018-08-01 09:10:55 -0700

    Rename

--- html/src/build-string.js
+++ html/src/build-string.js
@@ -1,11 +1,10 @@
 import buildSearchOrder from './build-search-order';
 import findMatchIndexes from './find-indexes';
 import AnnotateCore from 'repo/core';
-import findMarkers from './find-markers';
 import wrapMatches from './wrap';
 
 /**
- * @type {object} InsertAnnotationsIntoHTMLOptions
+ * @type {object} BuildHTMLStringOptions
  * @prop {string} html - The HTML string to insert annotations into.
  * @prop {AnnotationMarkers} markers - An annotation set
  * @prop {number} chapter - Index of the chapter in the book.
@@ -26,7 +25,7 @@ import wrapMatches from './wrap';
 /**
  * Finds and highlights an annotation set's items within the ebook's rendered
  * HTML.
- * @param {InsertAnnotationsIntoHTMLOptions} opt
+ * @param {BuildHTMLStringOptions} opt
  * @return {string} The modified HTML.
  */
 export default function(opt) {
